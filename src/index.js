ddeb16c21dd98ed1667e6bdb87e3859d1821cf99
Paulo Machado <pauloalexandre3d@gmail.com>
2019-10-19 12:33:29 +0100

    refactoring: Extract business logic

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import userLocation from './useLocation';
 
 const App = () => {
-  const [lat, setLat] = useState(null);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition(
-      position => setLat(position.coords.latitude),
-      err => setErrorMessage(err.message)
-    );
-  }, []);
-
+  const [lat, errorMessage] = userLocation();
   let content;
   if (errorMessage) {
     content = <div>Error: {errorMessage}</div>;
